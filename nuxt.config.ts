0c4e6983ae65e2a9dec12e4d3b3e572b1b4fdedc
Neil <me@neilrichter.com>
2024-05-01 01:32:25 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  nitro: {
+    experimental: {
+      tasks: true,
+    },
+  },
+  runtimeConfig: {
+    public: {
+      hostname: 'http://localhost:3000',
+    },
+    spotify: {
+      redirectUri: '/api/auth/callback',
+      clientId: '',
+      clientSecret: '',
+      refreshToken: '',
+    }
+  }
+})
